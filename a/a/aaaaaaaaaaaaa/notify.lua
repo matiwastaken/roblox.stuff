960c7b6f9b34047cdf4e9f68a76d012c79e17397
mateusz <james.hetfield.official.yeah@gmail.com>
2025-03-06 04:05:38 -0800

    Create notify.lua

---
+++ a/a/aaaaaaaaaaaaa/notify.lua
@@ -0,0 +1,12 @@
+-- just a notification script
+-- for AutoExec 3.0.0
+-- mateusz (2025/03/06)
+
+local StarterGui = game:GetService("StarterGui")
+
+StarterGui:SetCore("SendNotification", {
+    Title = "AutoExec 3.0.0 Loaded!";
+    Text = "Thanks for using AutoExec 3.0.0. all credits are in the script!"; 
+    Icon = "rbxassetid://89552406613017"; 
+    Duration = 5; 
+})
